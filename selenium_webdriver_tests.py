4e3667b9db4078e6ec3c088a978b9e6ec32ca094
Ots <omar.teresa@brainly.com>
2021-10-31 01:47:40 +0200

    Adding the 2nd TC, register account

--- selenium_webdriver_tests.py
+++ selenium_webdriver_tests.py
@@ -4,6 +4,7 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.select import Select
 from webdriver_manager.chrome import ChromeDriverManager
 
 
@@ -13,7 +14,7 @@ class TestSeleniumWebDriver(unittest.TestCase):
         s = Service(ChromeDriverManager().install())
         self.driver = webdriver.Chrome(service=s)
         self.driver.get(url="http://automationpractice.com/index.php")
-        self.driver.implicitly_wait(6)
+        self.driver.implicitly_wait(15)
 
     def tearDown(self) -> None:
         self.driver.quit()
@@ -30,6 +31,47 @@ class TestSeleniumWebDriver(unittest.TestCase):
         self.assertEqual(len(nodes), 5)
         # print(len(nodes))
         # time.sleep(20)
+
+    def test_registration_success(self):
+        self.driver.find_element(By.XPATH, "//a[@class='login']").click()
+        self.driver.find_element(By.ID, "email_create").send_keys("otsfake+04@gmail.com")
+        self.driver.find_element(By.ID, "SubmitCreate").click()
+        self.assertTrue(self.driver.title == 'Login - My Store')
+        self.driver.find_element(By.ID, "id_gender1").click()
+        self.driver.find_element(By.ID, "customer_firstname").send_keys("firstName")
+        self.driver.find_element(By.ID, "customer_lastname").send_keys("LastName")
+        self.driver.find_element(By.ID, "passwd").send_keys("Test123")
+
+        self.select_days = Select(self.driver.find_element(By.ID, "days"))
+        self.select_days.select_by_value("15")
+
+        self.select_month = Select(self.driver.find_element(By.ID, "months"))
+        self.select_month.select_by_value("1")
+
+        self.select_year = Select(self.driver.find_element(By.ID, "years"))
+        self.select_year.select_by_value("2000")
+
+        self.driver.find_element(By.ID, "address1").send_keys("address 1")
+        self.driver.find_element(By.ID, "city").send_keys("City")
+        self.driver.find_element(By.ID, "address1").send_keys("address 1")
+
+        self.select_state = Select(self.driver.find_element(By.ID, "id_state"))
+        self.select_state.select_by_value("1")
+
+        self.driver.find_element(By.ID, "postcode").send_keys("12123")
+        self.driver.find_element(By.ID, "phone_mobile").send_keys("511111111")
+        self.driver.find_element(By.ID, "submitAccount").click()
+        self.assertTrue(self.driver.current_url == "http://automationpractice.com/index.php?controller=my-account")
+        self.assertTrue(self.driver.find_element(By.XPATH, "//span[text()='firstName LastName']"))
+        self.driver.find_element(By.XPATH, "//*[@title='Log me out']").click()
+        self.assertTrue(self.driver.find_element(By.XPATH, "//*[@title='Log in to your customer account']"))
+
+        # time.sleep(5)
+
+
+
+
+
 
 
 
