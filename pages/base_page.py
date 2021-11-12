cf5dd52a7710411a48fef1b94565e97d0f6f9ae0
Ots <omar.teresa@brainly.com>
2021-11-12 16:30:54 +0100

    fixing hover_element method in BasePage

--- pages/base_page.py
+++ pages/base_page.py
@@ -63,17 +63,17 @@ class BasePage:
     def hover_element(self, by_locator):
         self.explicitly_wait.until(expected_conditions.element_to_be_clickable(by_locator),
                                    message=f"'{by_locator}' cannot hover over the element")
-        # hover_element = self.get_element(by_locator)
+        hover_element = self.get_element(by_locator)
         action = ActionChains(self.driver)
-        action.move_to_element(*by_locator)
+        action.move_to_element(hover_element)
         action.perform()
 
     def hover_click(self, by_locator):
         self.explicitly_wait.until(expected_conditions.presence_of_element_located(by_locator),
                                    message=f"'{by_locator}' cannot hover and click over the element")
-        # element_to_click = self.get_element(by_locator)
+        element_to_click = self.get_element(by_locator)
         action = ActionChains(self.driver)
-        action.click(*by_locator)
+        action.click(element_to_click)
         action.perform()
 
     def select_dropdown_value(self, by_locator, value):
